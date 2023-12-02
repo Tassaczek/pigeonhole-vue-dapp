be425fd45d0cde267ec1f623433e2bc811a757fa
Tassak <damian@mythe.pl>
2023-12-02 13:06:31 +0100

    Add new Event route

--- frontend/vue-dapp/src/router/index.ts
+++ frontend/vue-dapp/src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import ConnectWallet from '@views/ConnectWallet.vue';
 import Dashboard from '@views/Dashboard.vue';
+import Event from '@views/Event.vue';
 
 const routes = [
   { path: '/', component: ConnectWallet },
   { path: '/dashboard', component: Dashboard },
+  { path: '/event', component: Event },
 ];
 
 const router = createRouter({
